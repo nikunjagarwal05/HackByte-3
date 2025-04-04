64de0db297bba6765b70231c5d816f8d712eadb2
Nikunj Agarwal <180705nikunj@gmail.com>
2025-04-04 10:08:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HackByte-3
+ 
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='bg-black h-[100vh]'>
+      <div className='flex justify-center items-center text-2xl'>
+        <ul className='text-white flex justify-center items-start flex-col'>
+          <li>Landing page: functionality of the app</li>
+          <li>Login/ Signup</li>
+          <li>Levels: progress related, Health: streak related, Coins: for re-tests, Progress Bar, Streak</li>
+          <li>accent color of the page, sprite color and customization</li>
+          <li>Gemini chatbot for problem solving </li>
+          <li>Video lecture for navigation</li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
